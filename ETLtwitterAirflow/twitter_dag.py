5afa494160f954f337971fd50be969095669e681
jdpalmad <juandpalmad@gmail.com>
2023-01-09 21:02:12 -0500

    Airflow DAG file commented

--- ETLtwitterAirflow/twitter_dag.py
+++ ETLtwitterAirflow/twitter_dag.py
@@ -5,28 +5,33 @@ from airflow.utils.dates import days_ago
 from datetime import datetime
 from twitter_etl import run_twitter_etl
 
+# We define owner as owner and in case of having more DAG files 
+# we would set depends_on_past as True, also you can modify the alerts.
+
 default_args = {
-    'owner': 'airflow',
+    'owner': owner,
     'depends_on_past': False,
-    'start_date': datetime(2020, 11, 8),
-    'email': ['airflow@example.com'],
+    'start_date': datetime(2023, 1, 8),
+    'email': [airflow_email],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=1)
 }
 
+#Here we set the DAGS. Go to ./README.md for more.
+
 dag = DAG(
     'twitter_dag',
     default_args=default_args,
-    description='Our first DAG with ETL process!',
+    description='ETL process',
     schedule_interval=timedelta(days=1),
 )
 
 dag = DAG(
     'twitter_dag',
     default_args=default_args,
-    description='My first etl code'
+    description='etl code',
 )
 
 run_etl = PythonOperator(
